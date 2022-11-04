a080678807633293e067c2687c446722d3c38143
febyanahutauruk <febyanahutauruk@student.telkomuniversity.ac.id>
2022-11-04 16:50:24 +0700

    modul1
    prak oop modul 1

---
+++ OOP_FATH_CIKY_1202210411_SSMODUL1/Server.java
@@ -0,0 +1,15 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        public static void main (String [] args){
+            System.out.print("==================================");
+            System.out.print("\n 1. Bakso");
+            System.out.print("\n 2. Es Teh");
+            System.out.print("\n 3. Brownies");
+            System.out.print("==================================");
+        }
+    }
