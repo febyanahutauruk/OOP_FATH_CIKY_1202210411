54bf7cf2b631346fbaac615253a6fef4d1bb82b6
CIKY FEBYANA TAMARIA HUTAURUK <108115860+febyanahutauruk@users.noreply.github.com>
2022-12-04 23:01:07 +0700

    Delete file

--- Database.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.ArrayList;
-
-public class Database {
-    Scanner input = new Scanner(System.in);
-            System.out.print("Choose a number: ");
-            int number = input.nextInt();
-
-}
